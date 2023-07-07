27a6bfd543c460eb349cf4910bca172badb9bb75
Marek Smieja <mareksmieja@gmail.com>
2023-07-07 23:21:52 +0900

    Add different type of charts

--- src/layout/ChartsView.tsx
+++ src/layout/ChartsView.tsx
@@ -15,6 +15,12 @@ import { ProgressSpinner } from "primereact/progressspinner";
 import { Chart } from "primereact/chart";
 import { SettingsContext } from "../../src/lib/SettingsContext";
 import { dateFormatter } from "../../src/lib/dateFormatter";
+import { SelectButton, SelectButtonChangeEvent } from "primereact/selectbutton";
+
+interface ChartTypeOption {
+  icon: string;
+  value: string;
+}
 
 const { ipcRenderer } = window.require("electron");
 
@@ -30,6 +36,20 @@ const ChartsView = (): ReactElement => {
   const [chartData, setChartData] = useState({});
   const [chartOptions, setChartOptions] = useState({});
   const { settings }: AppSettingsContext = useContext(SettingsContext);
+  const [chartType, setChartType] = useState<string>("bar");
+  const chartTypeOptions: Array<ChartTypeOption> = [
+    { icon: "pi pi-chart-bar", value: "bar" },
+    { icon: "pi pi-chart-line", value: "line" },
+    { icon: "pi pi-chart-pie", value: "pie" },
+  ];
+
+  const chartTypeButtonTemplate = (option: ChartTypeOption) => {
+    return <i className={`m-auto ${option.icon}`}></i>;
+  };
+
+  useEffect(() => {
+    console.log(chartType);
+  }, [chartType]);
 
   ipcRenderer.once(
     AppEvents.LOAD_AVAILABLE_EXPENSES_DATES_RESPONSE,
@@ -93,6 +113,7 @@ const ChartsView = (): ReactElement => {
         labels: expenseTypes,
         datasets: [
           {
+            label: "Expenses",
             data: expenseAmounts,
             backgroundColor: [
               documentStyle.getPropertyValue("--blue-500"),
@@ -135,6 +156,26 @@ const ChartsView = (): ReactElement => {
       ipcRenderer.send(AppEvents.LOAD_EXPENSES, selectedExpenseDate);
   }, [selectedExpenseDate]);
 
+  useEffect(() => {
+    setChartOptions({
+      plugins: {
+        legend: {
+          labels: {
+            usePointStyle: true,
+          },
+        },
+      },
+      scales: {
+        x: {
+          display: chartType === "pie" ? false : true,
+        },
+        y: {
+          display: chartType === "pie" ? false : true,
+        },
+      },
+    });
+  }, [chartType]);
+
   return (
     <div className="flex flex-column align-items-center mt-4">
       {isLoadingDates ? (
@@ -162,7 +203,18 @@ const ChartsView = (): ReactElement => {
           </div>
           {selectedExpenseDate ? (
             <>
-              {isLoadingExpenses && !chartData ? (
+              <div className="card flex justify-content-center mb-5">
+                <SelectButton
+                  value={chartType}
+                  onChange={(e: SelectButtonChangeEvent) =>
+                    e.value !== null && setChartType(e.value)
+                  }
+                  itemTemplate={chartTypeButtonTemplate}
+                  optionLabel="value"
+                  options={chartTypeOptions}
+                />
+              </div>
+              {isLoadingExpenses && !chartData && !chartType ? (
                 <div className="w-full h-5 flex flex-column align-items-center">
                   <ProgressSpinner
                     className="mt-8"
@@ -174,7 +226,7 @@ const ChartsView = (): ReactElement => {
                 </div>
               ) : (
                 <Chart
-                  type="pie"
+                  type={chartType}
                   data={chartData}
                   options={chartOptions}
                   className="w-full md:w-30rem"
